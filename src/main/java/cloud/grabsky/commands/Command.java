b8c1616df6a829b861ac19792a4e574062c67be8
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2023-08-08 20:55:59 +0200

    introduce @Command and @Dependency as a new way to define commands

---
+++ src/main/java/cloud/grabsky/commands/Command.java
@@ -0,0 +1,25 @@
+package cloud.grabsky.commands;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Command {
+
+    @NotNull String name();
+
+    @Nullable String[] aliases() default "";
+
+    @Nullable String permission() default "";
+
+    @Nullable String usage() default "";
+
+    @Nullable String description() default "";
+
+}
